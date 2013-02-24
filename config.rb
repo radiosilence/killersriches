cc22e955ad459c353955e2314c7d18b67a59034e
James Cleveland <jamescleveland@gmail.com>
2013-02-24 15:54:35 +0000

    Move config.rb to project root

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ require "zurb-foundation"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "_wwwroot/stylesheets"
+sass_dir = "_wwwroot/sass"
+images_dir = "_wwwroot/images"
+javascripts_dir = "_wwwroot/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
